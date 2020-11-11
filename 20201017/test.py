dcd46495b5fe04a72b543a5f24b0567138fc6990
Listen-Y <1604053140@qq.com>
2020-11-11 11:45:46 +0800

    单元测试

---
+++ 20201017/test.py
@@ -0,0 +1,17 @@
+import unittest
+
+
+class TestStringMethods(unittest.TestCase):
+
+    def test_upper(self):
+        print("test_upper")
+
+    def test_isupper(self):
+        print("test_isupper")
+
+    def test_split(self):
+        print("test_split")
+
+
+if __name__ == '__main__':
+    unittest.main()
